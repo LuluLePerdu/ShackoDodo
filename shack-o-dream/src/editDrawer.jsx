b4cf92889563ccc39402ac84a23dbe9c3318a618
JulietteCordey <100165968+JulietteCordey@users.noreply.github.com>
2025-11-01 14:55:59 -0400

    Ajout drawer

---
+++ shack-o-dream/src/editDrawer.jsx
@@ -0,0 +1,67 @@
+import Dialog from '@mui/material/Dialog';
+import {DialogTitle, IconButton, styled} from "@mui/material";
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Drawer from '@mui/material/Drawer';
+import { MdClose } from 'react-icons/md';
+import TextareaAutosize from "@mui/material/TextareaAutosize";
+
+export default function EditDrawer({ open, onClose, data }) {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    var jsonData = JSON.stringify(data, null, 2);
+
+    const DrawerHeader = styled('div')(({ theme }) => ({
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-start',
+    }));
+
+    return (
+        <>
+            <Drawer
+                sx={{
+                    width: 600,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: 600
+                    },
+                }}
+                variant="persistent"
+                anchor="right"
+                open={open}
+            >
+                <DrawerHeader>
+                    <Button color="cancel" onClick={onClose}><MdClose/></Button>
+
+                </DrawerHeader>
+                <form onSubmit={handleSubmit} id="edit-request-form" style={{ marginRight: 25, marginLeft: 25}}>
+                    <TextField
+                        multiline
+                        rows={20}
+                        required
+                        margin="dense"
+                        id="req"
+                        name="req"
+                        label="Requête"
+                        type="text"
+                        fullWidth
+                        sx={{ '.MuiInputBase-input': {
+                                //fontFamily: 'monospace',
+                                fontFamily: '"Cascadia Code"'// Change font family
+                            },
+                        }}
+                        value={jsonData}
+                    />
+                </form>
+                <Button color="primary" type="submit" form="edit-request-form">Modifier</Button>
+            </Drawer>
+        </>
+    );
+}
