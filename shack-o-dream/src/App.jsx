ca27c9d4b33c3f63e756d13bcd84ba412389e2b3
Audrey Barry <2135420@etu.cegep-lanaudiere.qc.ca>
2025-11-01 11:31:27 -0400

    Ajout boutons/tableau

--- shack-o-dream/src/App.jsx
+++ shack-o-dream/src/App.jsx
@@ -1,37 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
 
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import {Table, TableHead, TableRow} from "@mui/material";
+import StickyHeadTable from "./tableaux.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button color="secondary" variant="contained" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+          <Button><FaPlay/></Button>
+          <Button><FaPause/></Button>
+          <Button>Nouvel onglet</Button>
+          <StickyHeadTable />
+          <script type="module" src="/src/tableaux.jsx"></script>
+      </>
   )
 }
 
